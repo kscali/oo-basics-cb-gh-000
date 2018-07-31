37e6b7f1836975db6dd5afb47e98fb84bbf8dd34
Koy Saeteurn <kosatun@aol.com>
2018-07-31 04:19:18 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,15 @@
 # Make your shoe class here!
 class Shoe 
+  attr_reader :brand
   
+  attr_accessor :color :size :material :condition
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
+  def cobble
+    puts "The shoe is new"
+  end 
   
   
   
